7975be06af2a8708e5961bb74583e3309a822d34
Shreyas S Tantri <shreyastantrik29@gmail.com>
2024-08-03 14:21:37 +0530

    new commit

--- C/Students Marks Sum.c
+++ C/Students Marks Sum.c
@@ -8,7 +8,8 @@ int marks_summation(int* marks, int number_of_students, char gender) {
     int sum=0;
     if(gender=='b'){
         // int sumBoys=0;
-        for (int i=0; i<number_of_students; i=i+2) {
+        for (int i=0; i<number_of_students; i=i+2) 
+       {
                 sum += marks[i]; 
         }
         return sum;
